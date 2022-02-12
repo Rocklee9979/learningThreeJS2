f696fc61ddcde878022e098a1a1cb4087d17f9b5
Rocklee9979 <48349639+Rocklee9979@users.noreply.github.com>
2022-02-12 12:51:32 +1100

    added stars on background and added light source to earth

--- react-3d/src/components/earth/Earth.jsx
+++ react-3d/src/components/earth/Earth.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useLoader } from '@react-three/fiber';
 import { TextureLoader } from 'three'
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, Stars } from '@react-three/drei';
 import * as THREE from 'three';
 
 import EarthDayMap from '../../assets/textures/8k_earth_daymap.jpg';
@@ -18,6 +18,14 @@ export default function Earth(props) {
     //cant use <div> because a canvas cant have a div components
     return <>
       {/* react three fiber has all components in lowercase */ }
+      <Stars
+        radius={ 300 }
+        depth={ 60 }
+        count={ 10000 }
+        factor={ 7 }
+        saturation={ 0 }
+        fade={ true }
+      />
       
       <OrbitControls
         enableZoom={ true }
@@ -27,7 +35,13 @@ export default function Earth(props) {
         enableRotate={ true }
         rotateSpeed={ 0.4 }
       />
-      <ambientLight intensity={ 1 } />
+
+      {/* <ambientLight intensity={ 1 } /> */}
+      <pointLight
+        color="#f5f7f5"
+        position={ [2, 0, 2] }
+        intensity={ 1.2}
+      />
       <mesh>
         <sphereGeometry args={ [2.005, 32, 32] } />
         <meshPhongMaterial
@@ -35,7 +49,9 @@ export default function Earth(props) {
           opacity={ 0.4 }
           depthWrite={ true }
           transparent={ true }
-          side={ THREE.DoubleSide}
+          side={ THREE.DoubleSide }
+          metalness={ 0.4 }
+          roughness={ 0.7}
         />
       </mesh>
       <mesh>
